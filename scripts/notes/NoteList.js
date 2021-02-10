fae49993a5f08852448ed97670ac5f59202d26de
Gabriel <bultaqi@gmail.com>
2021-02-10 14:10:52 -0500

    about to pull request into main

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,2 +1,15 @@
-import { NoteFrom } from './NoteForm.js'
 
+
+export function ContactList(){
+    getContact()
+    .then(() => {
+        const allTheContacts = copyOfContacts()
+
+        // This is an element we added!
+        const contentTarget = document.querySelector("#contacts")
+
+        // This does the same thing as printing with a for loop! We just use a .map() to loop over our collection and call the Note function instead of a for loop
+        
+        contentTarget.innerHTML = allTheContacts.map(singleContact => Contacts(singleContact))
+    })
+}
