9dfe947c6801c8f13161b4363cd964a6e61d482e
Gabriel <bultaqi@gmail.com>
2021-02-11 14:31:24 -0500

    fixed the dropdown for notes tied to criminal id instead of suspect notes in my database.

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,15 +1,49 @@
+import {getNotes, useNotes} from "./NoteDataProvider.js"
+import {Note} from "./Note.js"
+import {getCriminals, useCriminals} from "../criminals/CriminalProvider.js"
 
-
-export function ContactList(){
-    getContact()
+//Function that will list all the notes that exist in the local server
+export const NoteList = () => {
+    getNotes()
+    .then(getCriminals)
     .then(() => {
-        const allTheContacts = copyOfContacts()
+        const allTheNotes = useNotes()
+        const allTheCriminals = useCriminals()
 
-        // This is an element we added!
-        const contentTarget = document.querySelector("#contacts")
+        const noteContainer = document.querySelector("#the-box")
 
-        // This does the same thing as printing with a for loop! We just use a .map() to loop over our collection and call the Note function instead of a for loop
         
-        contentTarget.innerHTML = allTheContacts.map(singleContact => Contacts(singleContact))
+        const arrayOfNotesWithCriminalNames = allTheNotes.map(singleNote => {
+
+            const relatedCriminal = allTheCriminals.find(criminal => criminal.id === +singleNote.criminalId)
+            
+            const htmlString = Note(singleNote, relatedCriminal)
+
+            return htmlString
+
+            
+        })
+        
+        
+        
+        const StringOfAllNewNotes = arrayOfNotesWithCriminalNames.join("")
+
+        noteContainer.innerHTML = StringOfAllNewNotes
+
+
+        //this will erase the other content on the page when the notes are printed
+        let criminalListContainer = document.querySelector(".criminal-list");
+        criminalListContainer.innerHTML = ``;
+        let facilityContainer = document.querySelector(".facility-list");
+        facilityContainer.innerHTML = ``;
+        let officerContainer = document.querySelector(".officer-list");
+        officerContainer.innerHTML = ``;
+        
     })
+    
 }
+
+document.querySelector("#notes-nav-link").addEventListener("click", () => {
+    // invoke the function that prints the criminals
+    NoteList()
+})
